c7683703d97f37073630b06d077f5ddee9b18e04
kadirzkaya <bmkozkaya@gmail.com>
2022-09-19 13:14:43 +0300

    Öğrenci not sistemi

--- src/Course.java
+++ src/Course.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class Course {
+public class Course {
+    Teacher courseTeacher;
+    String name;
+    String code;
+    String prefix;
+    int note;
+    int verbalNote;
+    double verbalRatio;
+
+    public Course(String name, String code, String prefix, double verbalRatio) {
+        this.name = name;
+        this.code = code;
+        this.prefix = prefix;
+        this.verbalRatio=verbalRatio;
+        this.note = 0;
+        this.verbalNote=0;
+    }
+
+    public void addTeacher(Teacher t) {
+        if (this.prefix.equals(t.branch)) {
+            this.courseTeacher = t;
+            System.out.println("İşlem başarılı");
+        } else {
+            System.out.println(t.name + " Akademisyeni bu dersi veremez.");
+        }
+    }
+
+    public void printTeacher() {
+        if (courseTeacher != null) {
+            System.out.println(this.name + " dersinin Akademisyeni : " + courseTeacher.name);
+        } else {
+            System.out.println(this.name + " dersine Akademisyen atanmamıştır.");
+        }
+    }
 }
--- src/Student.java
+++ src/Student.java
@@ -1,2 +1,89 @@
-package PACKAGE_NAME;public class Student {
+public class Student {
+    String name,stuNo;
+    int classes;
+    Course mat;
+    Course fizik;
+    Course kimya;
+    double avarage, fizAvg, matAvg, kimAvg;
+    boolean isPass;
+
+
+    Student(String name, int classes, String stuNo, Course mat,Course fizik,Course kimya) {
+        this.name = name;
+        this.classes = classes;
+        this.stuNo = stuNo;
+        this.mat = mat;
+        this.fizik = fizik;
+        this.kimya = kimya;
+        calcAvarage();
+        this.isPass = false;
+    }
+
+
+    public void addBulkExamNote(int mat, int fizik, int kimya) {
+
+        if (mat >= 0 && mat <= 100) {
+            this.mat.note = mat;
+        }
+
+        if (fizik >= 0 && fizik <= 100) {
+            this.fizik.note = fizik;
+        }
+
+        if (kimya >= 0 && kimya <= 100) {
+            this.kimya.note = kimya;
+        }
+
+    }
+
+    public void addBulkVerbalNote(int mat, int fizik, int kimya) {
+
+        if (mat >= 0 && mat <= 100) {
+            this.mat.verbalNote = mat;
+        }
+
+        if (fizik >= 0 && fizik <= 100) {
+            this.fizik.verbalNote = fizik;
+        }
+
+        if (kimya >= 0 && kimya <= 100) {
+            this.kimya.verbalNote = kimya;
+        }
+
+    }
+
+    public void isPass() {
+        if (this.mat.note == 0 || this.fizik.note == 0 || this.kimya.note == 0) {
+            System.out.println("Notlar tam olarak girilmemiş");
+        } else {
+            this.isPass = isCheckPass();
+            printNote();
+            System.out.println("Ortalama : " + this.avarage);
+            if (this.isPass) {
+                System.out.println("Sınıfı Geçti. ");
+            } else {
+                System.out.println("Sınıfta Kaldı.");
+            }
+        }
+    }
+
+    public void calcAvarage() {
+        this.matAvg= (this.mat.note*(1- this.mat.verbalRatio)+(this.mat.verbalNote*this.mat.verbalRatio));
+        this.fizAvg= (this.fizik.note*(1- this.fizik.verbalRatio)+(this.fizik.verbalNote*this.fizik.verbalRatio));
+        this.kimAvg= (this.kimya.note*(1- this.kimya.verbalRatio)+(this.kimya.verbalNote*this.kimya.verbalRatio));
+        this.avarage = (this.matAvg + this.fizAvg + this.kimAvg) / 3;
+    }
+
+    public boolean isCheckPass() {
+        calcAvarage();
+        return this.avarage > 55;
+    }
+
+    public void printNote(){
+        System.out.println("=========================");
+        System.out.println("Öğrenci : " + this.name);
+        System.out.println("Matematik Notu : " + this.matAvg);
+        System.out.println("Fizik Notu : " + this.fizAvg);
+        System.out.println("Kimya Notu : " + this.kimAvg);
+    }
 }
